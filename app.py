55f69308b084b29c1bdde33a4747aabe31718bbe
com <elgesto@gmail.com>
2018-06-19 02:53:42 -0400

    Add buttons

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+import telegram
 import logging
+from logging import handlers
 from utils import *
 from scipy.spatial.distance import cosine, euclidean
 import numpy as np
@@ -16,11 +19,36 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-# handler = logging.handlers.RotatingFileHandler('foo.log', maxBytes=(1048576*5), backupCount=7)
-# logger.addHandler(handler)
+handler = handlers.RotatingFileHandler('foo.log', maxBytes=(1048576*5), backupCount=7)
+logger.addHandler(handler)
 
 def start(bot, update):
-    update.message.reply_text('Please type the product query')
+    keyboard = [[InlineKeyboardButton("Catalog", callback_data='1'),
+                 InlineKeyboardButton("Orders", callback_data='2'),
+                 InlineKeyboardButton("FAQ", callback_data='3')]]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+
+#def start(bot, update):
+#    update.message.reply_text('Please type the product query')
+
+def button(bot, update):
+    query = update.callback_query
+
+    if query.data == '1':
+#      update.message.reply_text('Please type the product query.')
+      bot.edit_message_text(text="Please type the product query.", chat_id=query.message.chat_id, message_id=query.message.message_id)
+
+    if query.data == '2':
+      #update.message.reply_text('You do not have orders.') 
+      bot.edit_message_text(text="You do not have orders.", chat_id=query.message.chat_id, message_id=query.message.message_id)
+
+    if query.data == '3':
+   #   update.message.reply_text('Please type your question.')
+      bot.edit_message_text(text="Please type your question.", chat_id=query.message.chat_id, message_id=query.message.message_id)
+
 
 def help(bot, update):
     update.message.reply_text('Please type the product query')
@@ -32,6 +60,8 @@ def echo(bot, update):
 
     text_mean_emb = emb_mean.transform([nlp(text)])[0]
     text_tfidf_emb = emb_tfidf.transform([nlp(text)])[0]
+
+    bot.send_chat_action(chat_id=update.message.chat_id, action=telegram.ChatAction.TYPING)
 
     results_mean = [cosine(text_mean_emb, emb) if np.sum(emb)!=0 else math.inf for emb in data_mean]
     results_tfidf = [cosine(text_tfidf_emb, emb) if np.sum(emb)!=0 else math.inf for emb in data_tfidf]
@@ -75,6 +105,7 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CallbackQueryHandler(button))
     dp.add_handler(CommandHandler("help", help))
 
     dp.add_handler(MessageHandler(Filters.text, echo))
@@ -91,7 +122,8 @@ if __name__ == '__main__':
     # nlp = spacy.load('en_core_web_md', parser=False)
     nlp = spacy.load('en_vectors_web_lg', parser=False)
 
-    with open("processed.pickle.big", "rb") as handle:
+    with open("processed.pickle.home.big", "rb") as handle:
+    #with open("processed.pickle.big", "rb") as handle:
         doc_bytes, vocab_bytes = pickle.load(handle)
         print('pickle was loaded')
 
